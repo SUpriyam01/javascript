c3eca07d54a69bfa291c65028fa2e343ca9f8eb6
SUpriyam01 <supriyam1001@gmail.com>
2025-03-04 00:04:22 +0530

    variable

---
+++ 01_variable.js
@@ -0,0 +1,15 @@
+const accountId = 55
+let accountEmail = "supriyam@google.com"
+var accountPassword = "12345"
+accountCity = "jaipur"
+
+//accountId = 2 // not allowed
+accountEmail ="raj@gmail.com"
+accoubtPassword = "34567"
+accountCity = "patna"
+console.log(accountId)
+let accountState;
+/*
+prefer not to use because of  issue in block scope and function scope
+*/
+console.table([accountId,accountEmail,accoubtPassword,accountCity,accountState])
