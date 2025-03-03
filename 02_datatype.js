3b9fc1ff19560e696408a8351359938078b427a4
SUpriyam01 <supriyam1001@gmail.com>
2025-03-04 00:04:37 +0530

    datatype

---
+++ 02_datatype.js
@@ -0,0 +1,22 @@
+"use strict"; //treat all js code as newer version
+
+//alert(3+4) //we are using nodejs, nor browser
+
+console.log(3+4) //code readability should be high
+
+console.log("supriyam")
+
+let name = "supriyam" //string datatype
+let age = 18 //num datatype
+let isLoggedIn = false //boolean datatype
+
+//number => 2 to power 53
+//bigint
+//string =>""
+//boolean => standlone value
+//undefined =>
+//symbol  => unique
+
+//object
+
+console.log(typeof undefined);
